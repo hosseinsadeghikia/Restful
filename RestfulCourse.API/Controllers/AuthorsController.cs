9076cb59c2a1dd841e2df765dc5f02dcb693d11c
Hossein Sadeghi <h.sadeghi@H-SADEGHI-PRD>
2021-03-06 07:57:04 +0330

    Initial

---
+++ RestfulCourse.API/Controllers/AuthorsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using RestfulCourse.API.Services;
+
+namespace RestfulCourse.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
